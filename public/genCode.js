e44bc2d3a521b8e364a10c6e836605e5b9f1fa50
Airam Navas <alu0100618426@ull.edu.es>
2017-06-04 01:22:43 +0100

    Actualizando codeGen

--- public/genCode.js
+++ public/genCode.js
@@ -10,8 +10,7 @@ module.exports = () => {
 }; // end prefix
 
 let suffixTemplate  = function() {
-   return `;
-     return sym;
+   return `return sym;
   }
   catch(e) {
     let err = e.message.replace(/sym\\.(\\w+)/g, '$1');
@@ -27,12 +26,118 @@ let genCode = function(tree) {
    var prefix = prefixTemplate();
    var suffix = suffixTemplate();
    /* traverse the tree producing translation */
-   let js = prefix+translate()+suffix;
+   let js = prefix+translate(tree)+suffix;
    //return beautify(js, { indent_size: 2 });
    return js;
 };
 
+let translate2 = function(obj, result) {
+  console.log(result.type);
+  switch(result.type) {
+      case "block":         obj.code += "{\n     ";
+                            for(let i = 0; i < result.contents.length; i++){
+                              obj.code += "     ";
+                              translate2(obj, result.contents[i]);
+                            }
+                            obj.code += "\n     }";
+          break;
+      case "IF":
+          break;
+      case "while":         obj.code += "while (";
+                            translate2(obj, result.check);
+                            obj.code += ")";
+                            translate2(obj, result.contents);
+                            if(result.else != null){
+                              obj.code += " else ";
+                              translate2(obj, result.else);
+                            }
+          break;
+      case "for":           obj.code += "for (";
+                            translate2(obj, result.start);
+                            translate2(obj, result.check);
+                            translate2(obj, result.iterate);
+                            obj.code += ")";
+                            translate2(obj, result.contents);
+          break;
+      case "parcondition":
+          break;
+      case "assign":        for(let i = 0; i < result.assignations.length; i++){
+                              obj.code += result.assignations[i].id + " = ";
+                              translate2(obj, result.assignations[i].to);
+                            }
+                            obj.code += ";";
+          break;
+      case "declaration":   obj.code += "var ";
+                            for(let i = 0; i < result.assignations.length; i++){
+                              obj.code += result.assignations[i].id + " = ";
+                              translate2(obj, result.assignations[i].to);
+                              if(result.assignations.length > 1 && i < result.assignations.length - 1)
+                                obj.code += ", ";
+                            }
+                            obj.code += ";";
+          break;
+      case "function":      obj.code += "function " + result.functionName;
+                            obj.code += "(";
+                            if(result.params.length > 0)
+                              for(let i = 0; i < result.params.length; i++){
+                                translate2(obj, result.params[i]);
+                                if(result.params.length > 1 && i < result.params.length - 1)
+                                  obj.code += ", ";
+                              }
+                            obj.code += ")";
+                            translate2(obj, result.contents);
+          break;
+      case "parameter":     obj.code += result.vartype + " " + result.id;
+          break;
+      case "return":        obj.code += "return ";
+                            if(result.returnValue != null)
+                              translate2(obj, result.returnValue);
+                            obj.code += ";";
+          break;
+      case "class":
+          break;
+      case "classBlock":
+          break;
+      case "attribute":
+          break;
+      case "method":
+          break;
+      case "condition":
+          break;
+      case "expression":
+          break;
+      case "term":
+          break;
+      case "call":
+          break;
+      case "idAccess":
+          break;
+      case "arrayAccess":
+          break;
+      case "id":
+          break;
+      case "arguments":
+          break;
+      case "numeric":       obj.code += result.value;
+          break;
+      case "string":        obj.code += "\"" + result.value + "\"";
+          break;
+      case "bool":          obj.code += result.value;
+          break;
+      //TODO  array por hacer
+      default:
+  }
+  //console.log(obj.code);
+}
+
 let translate = function(tree) {
-    console.log("Translate");
-    return "var a = 0 ";
+    console.log("-----------");
+    console.log("-Translate-");
+    console.log("-----------");
+    var obj = { code: "   " };
+    for(let i = 0; i < tree.result.length; i++){
+      translate2(obj, tree.result[i]);
+      obj.code += "\n     ";
+    }
+    return obj.code;
 }
